e0aa35f20e8c6ccec53b0f1db6d0c948d826222f
Alexandr <chaushevak@icloud.com>
2020-04-03 18:16:19 +0300

    init taco-cloud

---
+++ Spring/taco-cloud/src/main/java/com/example/tacocloud/entity/Ingredient.java
@@ -0,0 +1,26 @@
+package com.example.tacocloud.entity;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class Ingredient {
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+}
